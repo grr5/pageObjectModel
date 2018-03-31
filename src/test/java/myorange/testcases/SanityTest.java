fcb11e3e696b4b13c9f7e61a1bc1c20f3e6fe2fd
Ravi <ravi@gmail.com>
2018-03-31 17:15:56 +0100

    changed in sanity class

--- src/test/java/myorange/testcases/SanityTest.java
+++ src/test/java/myorange/testcases/SanityTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+//this ic change
 public class SanityTest extends BaseTest {
     LoginPage loginPage;
     WelcomeAdminPage welcomeAdminPage;
